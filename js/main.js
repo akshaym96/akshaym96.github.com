1a16928cd420cb52be70e27f4d6073697e96a7b4
Mallipeddi Akshay <akshaym_96@yahoo.co.in>
2017-11-12 19:54:31 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,43 +1,5 @@
 $(document).ready( function() {
-  var SPACE = " ";
-  var DOT = ".";
-  var backgroundColors = ["#44AF69", "#F8333C", "#FCAB10", "#2B9EB3", "#2A2D34"];
-  var foregroundColors = ["white", "white", "white", "white", "white"];
-  var quoteContent = "";
-  var quoteAuthor = "";
-  var randomQuoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
-
-  var faQuoteStartRight = "<i class='fa fa-quote-right' aria-hidden='true'></i>";
-  var faQuoteStartLeft = "<i class='fa fa-quote-left' aria-hidden='true'></i>";
  
-	
-	function requestQuote () {
-    
-    $.ajax( {
-      url: randomQuoteUrl,
-      success: function(data) {
-
-        var firstQuote = data.shift();
-          var quoteContent_ = faQuoteStartLeft + SPACE + quoteContent + SPACE + faQuoteStartRight; // adding start and end quote marks to quote content
-
-     
-          $(".quote-content-row").html(quoteContent_);
-          $(".quote-author").html(firstQuote.title);
-
-          quoteAuthor = firstQuote.title;
-        <script type="text/javascript">
-
-         document.write("<h1>Hello member</h1>");
-
-</script>
-      },
-      cache: false
-    });
-  }
-	
-requestQuote();
-	
-	
 	// Logo
 	var $logo 	= $('#logo');
 	 if (location.href.indexOf("#") != -1) {
